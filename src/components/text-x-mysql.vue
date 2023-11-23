d04a0b8c29945588308422ca714aee388a21e885
BenzJ <jbcbenz@gmail.com>
2023-11-23 11:20:22 +0800

    feat: 暂时先去除行号还有行号重叠问题

--- src/components/text-x-mysql.vue
+++ src/components/text-x-mysql.vue
@@ -1,11 +1,21 @@
 <template>
- <el-dialog v-model="internalVisible" title="文件预览" width="80%" @close="closeDialog">
-    <codemirror v-model="code" :options="cmOption" />
+ <el-dialog v-model="internalVisible" title="文件预览" width="80%" :style="{ height: '600px' }" @close="closeDialog">
+    <codemirror v-model="code"
+     :options="cmOption" 
+     border
+     :height="400" 
+     class="CodeMirror"
+     />
+    <template #footer>
+        <el-button @click="closeDialog">保存</el-button>
+        <el-button @click="closeDialog">关闭</el-button>
+      </template>
   </el-dialog>
 </template>
 
 <script>
 import dedent from 'dedent';
+// import CodeMirror from "codemirror";
 import Codemirror from "codemirror-editor-vue3";
 import 'codemirror/theme/solarized.css';
 
@@ -24,8 +34,9 @@ export default {
       cmOption: {
         tabSize: 4,
         styleActiveLine: true,
-        lineNumbers: true,
-        line: true,
+        lineNumbers: false,
+        // gutters: ["CodeMirror-line", "my-custom-gutter"],
+        // line: true,
         mode: 'text/x-mysql',
         theme: 'solarized light',
       },
@@ -60,7 +71,21 @@ export default {
         this.$emit('update:dialogVisible', this.internalVisible);
       });
     },
+  },
+  // mounted() {
 
-  }
+  // }
 };
 </script>
+
+<style scoped>
+/* 样式可以根据需求自行定义 */
+.CodeMirror { text-align: left }
+.CodeMirror-line  {
+  padding-right: 30px !important;
+}
+.CodeMirror-linenumbers {
+  color: #000;       /* 行号颜色 */
+  font-size: 14px;   /* 行号字体大小 */
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
 import 'prismjs/themes/prism.css';
 import VueCodemirror from 'vue-codemirror'
+import { InstallCodemirro } from "codemirror-editor-vue3";
 import 'codemirror/lib/codemirror.css'
 
 const app = createApp(App);
 app.use(ElementPlus);
+app.use(InstallCodemirro);
 app.use(VueCodemirror);
 app.use(router); // 确保在调用 use 之前导入了 router 模块
 app.mount('#app');
