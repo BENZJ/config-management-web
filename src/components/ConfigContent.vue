6128794526cb192c950ced45c3806ebb51766e76
BenzJ <jbcbenz@gmail.com>
2023-12-05 09:29:28 +0800

    feat: 创建文件模块添加

--- src/components/ConfigContent.vue
+++ src/components/ConfigContent.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div style="text-align: right;;  border: 1px solid #FFF; ">
-      <el-icon style="margin-right: 10px;" size="25" default="添加文件">
+      <el-icon style="margin-right: 10px;" size="25" default="添加文件" @click="showCreateFileDialog">
         <DocumentAdd />
       </el-icon>
       <el-icon style="margin-right: 10px;" size="25" default="打包下载">
@@ -11,12 +11,22 @@
     <!-- <p>当前的 ID 值是: {{ this.id }}</p> -->
     <div v-for="file in tableList" :key="file.id" class="file-list-item">
       <ConfigDetail :ref="configDetailRefs[file.id]" :fileId=file.id :fileName="file.fileName"
-        @open-edite-viewer="openEditeViewer" @open-edite-preview="openEditePreview" @add-row="addRow"/>
+        @open-edite-viewer="openEditeViewer" @open-edite-preview="openEditePreview" @add-row="addRow" />
     </div>
     <!-- 文件预览弹框 -->
     <EditeViewer v-model:dialogVisible="fileViewerDialogVisible" :fileId="fileId" :fileContent="currentFileContent"
       :recordId="rowId" :recordName="handerUser" :recordRemark="remark" :editable="editable"
       @edite-row="reloadFileDetail" />
+
+    <!-- 创建文件对话框 -->
+    <el-dialog v-model="createFileDialogVisible" title="创建新文件" @close="closeDialog">
+      <el-form :model="newFileForm" label-width="80px" ref="newFileFormRef">
+        <el-form-item label="文件名称" prop="fileName">
+          <el-input v-model="newFileForm.fileName" />
+        </el-form-item>
+      </el-form>
+      <el-button @click="createFile">创建文件</el-button>
+    </el-dialog>
   </div>
 </template>
 
@@ -33,6 +43,10 @@ export default {
   data() {
     return {
       showTable: false,
+      createFileDialogVisible: false,
+      newFileForm: {
+        fileName: '',
+      },
       tableList: [],
       tableData: [],
       fileViewerDialogVisible: false,
@@ -85,16 +99,58 @@ export default {
         console.error('获取文件列表失败', error);
       }
     },
-    async getPreviewFile(id){
-      try{
-        console.log("预览文件id:",id)
-        const res = await get('/api/getFilePreview',{'id': id})
+    async getPreviewFile(id) {
+      try {
+        console.log("预览文件id:", id)
+        const res = await get('/api/getFilePreview', { 'id': id })
         this.currentFileContent = res.data
-      }catch(error){
+      } catch (error) {
         console.error('获取文件预览失败', error);
       }
     },
+    showCreateFileDialog() {
+      // 点击添加文件图标按钮时显示创建文件对话框
+      this.createFileDialogVisible = true;
+    },
+    createFile() {
+      // 在这里执行创建文件的逻辑，可以向后端发送请求等
+      console.log('创建文件：', this.newFileForm.fileName);
+      const form = this.$refs.newFileFormRef;
+      // 校验表单
+      form.validate(valid => {
+        if (valid) {
+          // 表单验证通过，执行创建文件的逻辑
+          const fileName = this.newFileForm.fileName;
 
+          // 检查文件名格式
+          if (/^[^\\/?%*:|<>\s]+[.][^\\/?%*:|<>\s]+$/.test(fileName)) {
+            // 文件名不包含特殊字符，且有正确的后缀
+            console.log('文件名格式正确，可以提交创建文件：', fileName);
+
+            // 在这里执行创建文件的逻辑，例如向后端发送请求等
+
+            // 创建完文件后关闭对话框
+            this.createFileDialogVisible = false;
+          } else {
+            // 文件名格式不正确，给出提示
+            this.$message.error('文件名格式不正确，确保文件名不包含特殊字符且有正确的后缀');
+          }
+        } else {
+          // 表单验证不通过，给出提示
+          this.$message.error('请输入文件名');
+        }
+      });
+      // 创建完文件后关闭对话框
+      this.createFileDialogVisible = false;
+    },
+    closeDialog() {
+      // 在关闭弹窗时清空相关数据
+      this.newFileForm.fileName = '';  // 清空文件名
+      // 其他需要清空的数据...
+
+      // 关闭弹窗
+      this.createFileDialogVisible = false;
+    },
     openEditeViewer(data) {
       console.log('监听到打开弹窗事件:', data);
       this.currentFileContent = data.content; // 更新当前文件内容
@@ -107,7 +163,7 @@ export default {
       this.remark = data.rowRemark;
     },
 
-    async openEditePreview(data){
+    async openEditePreview(data) {
       console.log('监听到打开预览弹框事件:', data);
       await this.getPreviewFile(data.fileId)
       // this.currentFileContent = data.content; // 更新当前文件内容
@@ -115,7 +171,7 @@ export default {
       this.editable = false;
     },
 
-    addRow(data){
+    addRow(data) {
       console.log('监听到打开预览弹框事件:', data);
       // this.currentFileContent = data.content; // 更新当前文件内容
       this.fileId = data.fileId;
@@ -123,7 +179,7 @@ export default {
       this.editable = true;
       this.handerUser = '';
       this.remark = '';
-      this.currentFileContent='';
+      this.currentFileContent = '';
     },
 
     reloadFileDetail(data) {
